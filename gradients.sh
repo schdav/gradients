9dbae7eb3af56473110267ed92afa4dd7f385f2f
David Schneider <schdav@users.noreply.github.com>
2021-02-18 21:38:51 +0100

    Improve code

--- gradients.sh
+++ gradients.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-mkdir gradients
-cd gradients
+dir="gradients"
+
+if [ ! -d "$dir" ]; then
+  mkdir $dir
+fi
+
+cd $dir
 
 for i in $(seq 1 $3); do
   r1=$(($RANDOM%256))
@@ -15,6 +20,8 @@ for i in $(seq 1 $3); do
   a=$(($RANDOM%181))
 
   name="gradient_${i}.png"
+
+  echo "Generating gradient $i of $3 ..."
 
   magick -size $1x$2 -define gradient:angle=$a gradient:'rgb('$r1','$g1','$b1')'-'rgb('$r2','$g2','$b2')' $name
 done
